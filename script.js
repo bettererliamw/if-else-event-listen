13b32c960482ce3ca6e43a3c412cd20d2865658f
Liam White <liamluigi7@gmail.com>
2024-10-15 21:47:16 +0000

    Finally fixed the bug

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 let namebox = document.getElementById('namebox');
 let username = document.getElementById('namebox').textContent;
 let response = document.getElementById('responsetext')
-let hiddenpage = document.getElementById('jameshating')
+let hiddenpage = document.getElementById('jameshating');
 
 function submitname() {
     let name = namebox.value
     console.log('"' + name + '" was submitted.')
-    if ( name === 'james' ) {
+    if ( name === 'james') {
         response.textContent = 'Your name is James, therefore we cannot grant you access to this website.';
-        console.log('Name was James');
+        console.log('Name was James, denying access to page.');
+        document.getElementById('jameshating').style.visibility = 'hidden';
     } else {
         response.textContent = 'Your name is not James, therefore we will now grant you access to this website.';
         console.log('Name was NOT James');
-        hiddenpage.style.visibility: visible
-        console.log('Opening page.');
+        document.getElementById('jameshating').style.visibility = 'visible';
+        console.log('Attempting to open page.');
     }
 
 }
